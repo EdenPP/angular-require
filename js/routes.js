02490cfeb974e9e5cf2366644bc70cae985891a5
易德 <yide@cnhuqi.com>
2017-12-28 11:55:08 +0800

    * 路由不指定控制器

--- js/routes.js
+++ js/routes.js
@@ -2,7 +2,6 @@ define(['js/app'], function (app) {
     app.config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/', {
             'templateUrl': 'module/index/index.html',
-            'controller': 'index-ctrl',
             'resolve': {
                 deps: app.loadMyJs([
                     'css!module/index/index',
@@ -11,7 +10,6 @@ define(['js/app'], function (app) {
             }
         }).when('/home', {
             'templateUrl': 'module/home/index.html',
-            'controller': 'home-ctrl',
             'resolve': {
                 deps: app.loadMyJs([
                     'css!module/home/index',
